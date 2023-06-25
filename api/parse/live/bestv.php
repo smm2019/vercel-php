a31bb68f6113203d5104e2b8f10301d3f83d28b3
tqx5201 <tqx5201@163.com>
2023-06-25 09:42:27 +0800

    Update bestv.php

--- api/parse/live/bestv.php
+++ api/parse/live/bestv.php
@@ -1,53 +1,62 @@
 <?php
-//百视通直播、回看
-//header('Content-type:text/plain;charset=utf-8');
-
-$domain = 'http://223.109.33.33/liveplay-kk.rtxapp.com';
-$domain = 'http://[2409:8c74:f100:1804::14]/liveplay-kk.rtxapp.com';
-$domain = 'http://223.111.117.11/liveplay-kk.rtxapp.com';
-
-
-$id = isset($_GET['id'])?$_GET['id']:'dsjpdhd@4000000';
+date_default_timezone_set("Asia/Shanghai");
+$id = isset($_GET['id'])?$_GET['id']:'dfyshd8m@8000000';
 $id = str_replace('@', '/', $id);
 $playseek = isset($_GET['playseek'])?$_GET['playseek']:'';
+$domain = 'http://223.111.117.11/liveplay-kk.rtxapp.com';
 if ($playseek != '') {
     $palyseek_arr = explode('-', $playseek);
     $s_time = $palyseek_arr[0];
     $e_time = $palyseek_arr[1];
-    //get_tvback_url_bestv($domain_rand, $id, $s_time, $e_time);
-
-    $url = sprintf('%s/live/program/live/%s/mnf.m3u8?starttime=%d&endtime=%d', $domain, $id, strtotime($s_time), strtotime($e_time));
-    //echo $url;exit;
-    header('Location:'.$url);
+    get_tvback($domain, $id, $s_time, $e_time);
 } else {
-    $url = sprintf('%s/live/program/live/%s/mnf.m3u8', $domain, $id);
-    //echo $url;exit;
-    header('Location:'.$url);
+    get_live($domain, $id);
 }
 
 
 
-//直播生成回看地址
-function get_tvback_url_bestv($domain, $ch_id = 'cctv1hd/4000000', $s_time, $e_time) {
+function get_live($domain, $id = 'dfyshd8m/8000000') {
+    $date = date('YmdH');
+    $timestamp = intval((time()-50)/10);
+    $stream = $domain.'/live/program/live/'.$id.'/'.$date.'/';
+    $current = "#EXTM3U"."\r\n";
+    $current .= "#EXT-X-VERSION:3"."\r\n";
+    $current .= "#EXT-X-TARGETDURATION:3"."\r\n";
+    $current .= "#EXT-X-MEDIA-SEQUENCE:{$timestamp}"."\r\n";
+    for ($i = 0; $i < 3; $i++) {
+        $current .= "#EXTINF:10.000,"."\r\n";
+        $current .= $stream.$timestamp.".ts"."\r\n";
+        $timestamp = $timestamp + 1;
+    }
+    header("Content-Type: text/plain");
+    //header('Content-Type: application/vnd.apple.mpegurl');
+    //header("Content-Disposition: attachment; filename=mnf.m3u8");
+    echo $current;
+}
+
+//回看
+function get_tvback($domain, $id, $s_time, $e_time) {
     $st = ceil(strtotime($s_time)/10);
     $et = ceil(strtotime($e_time)/10);
-    $http = $domain.'/live/program/live';
+    $http = $domain.'/live/program/live/';
     $m3u8 = '#EXTM3U'; $m3u8 .= PHP_EOL;
     $m3u8 .= '#EXT-X-VERSION:3'; $m3u8 .= PHP_EOL;
     $m3u8 .= '#EXT-X-TARGETDURATION:10'; $m3u8 .= PHP_EOL;
     $m3u8 .= '#EXT-X-MEDIA-SEQUENCE:'.$st; $m3u8 .= PHP_EOL;
     for ($i = $st; $i < $et; $i++) {
         $ymdh = date('YmdH', $i*10);
-        $url = sprintf('%s/%s/%s/%s.ts', $http, $ch_id, $ymdh, $i);
+        $url = sprintf('%s/%s/%s/%s.ts', $http, $id, $ymdh, $i);
         $m3u8 .= '#EXTINF:10,'; $m3u8 .= PHP_EOL;
         $m3u8 .= $url; $m3u8 .= PHP_EOL;
     }
     $m3u8 .= '#EXT-X-ENDLIST';
-    header('Content-Type: application/vnd.apple.mpegurl');
-    header("Content-Disposition: attachment; filename=mnf.m3u8");
+    header("Content-Type: text/plain");
+    //header('Content-Type: application/vnd.apple.mpegurl');
+    //header("Content-Disposition: attachment; filename=mnf.m3u8");
     echo $m3u8;
-    //return $url;
 }
+
+
 
 
 ?>
