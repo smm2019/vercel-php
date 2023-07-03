cfa04519815b8a3fd41921ef27a1351f57559332
tqx5201 <tqx5201@163.com>
2023-07-03 09:58:20 +0800

    Update tvb.php

--- api/parse/live/tvb.php
+++ api/parse/live/tvb.php
@@ -1,21 +1,5 @@
-<?php
-/*
-    .php?id=0&hq 無線新聞台1080P
-    .php?id=0 無線新聞台720P
-    .php?id=1 無線新聞台360P
-    .php?id=2&hq 無線財經·體育·資訊台1080P
-    .php?id=2 無線財經·體育·資訊台720P
-    .php?id=3 無線財經·體育·資訊台360P
-    .php?id=4&hq 事件直播頻道1 1080P（全清晰度）
-    .php?id=4 事件直播頻道1 720P
-    .php?id=5 事件直播頻道1 576P
-    .php?id=6&hq 事件直播頻道2 1080P（全清晰度）
-    .php?id=6 事件直播頻道2 720P
-    .php?id=7 事件直播頻道2 576P
-*/
-$id = $_GET['id'];
-$ids = ['I-NEWS','I-NEWS','I-FINA','I-FINA','NEVT1','NEVT1','NEVT2','NEVT2'];
-$hq = $_GET['hq'];
+$id = $_GET['id']??0;
+$ids = ['I-J','I-J2','C','C','I-NEWS','I-NEWS','I-FINA','I-FINA','NEVT1','NEVT1','NEVT2','NEVT2'];
 if(!isset($ids[$id])) {
     exit();
 };
@@ -31,14 +15,13 @@ $data = curl_exec($ch);
 curl_close($ch);
 $json = json_decode($data);
 $url = $json->content->url;
-if(isset($hq)) {
-    if($id == '4' || $id == '5' || $id == '6' || $id == '7') {
-        header('location:'.preg_replace('/&p=(.*?)$/','',$url->hd));
-    } else {
-        header('location:'.preg_replace('/&p=(.*?)$/','&p=3000',$url->hd));
-    };
-} else if($id == '0' || $id == '2' || $id == '4' || $id == '6') {
-    header('location:'.$url->hd);
-} else {
+if($id == '3' || $id == '5' ||$id == '7' || $id == '9' || $id == '11' || $id == '13') {
     header('location:'.$url->sd);
+} else {
+    if($id == '2' || $id == '4' || $id == '6' || $id == '8' || $id == '10') {
+        $r = preg_replace('/&p=(.*?)$/','',$url->hd);
+        header('location:'.$r);
+        exit();
+    };
+    header('location:'.$url->hd);
 };
