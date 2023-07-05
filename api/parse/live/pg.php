7880ee73e6a5456ede82b9f21c151e38a4a7033f
tqx5201 <tqx5201@163.com>
2023-07-05 18:48:09 +0800

    Update pg.php

--- api/parse/live/pg.php
+++ api/parse/live/pg.php
@@ -1,4 +1,14 @@
 <?php
+
+echo $_ENV(POSTGRES_URL);echo '<br>';
+echo $_ENV(POSTGRES_URL_NON_POOLING);echo '<br>';
+echo $_ENV(POSTGRES_PRISMA_URL);echo '<br>';
+echo $_ENV(POSTGRES_USER);echo '<br>';
+echo $_ENV(POSTGRES_HOST);echo '<br>';
+echo $_ENV(POSTGRES_PASSWORD);echo '<br>';
+echo $_ENV(POSTGRES_DATABASE);echo '<br>';
+
+
 $conn_string = "host=ep-restless-lake-843526.us-east-1.postgres.vercel-storage.com port=5432 dbname=verceldb user=default password=Cb49hWnYOtaK";
 $dbconn4 = pg_connect($conn_string);
 var_dump($dbconn4);
