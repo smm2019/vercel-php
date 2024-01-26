ebe1923bf6ef5baac321dbf86179b7635658698a
天仙婆婆 <tqx5201@163.com>
2024-01-26 20:41:53 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,20 @@
 <?php
-phpinfo();
+//phpinfo();
+$db = new SQLite3('/var/task/user/api/database/db.sqlite');
+
+$db->query('CREATE TABLE IF NOT EXISTS "visits" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "url" VARCHAR,
+    "time" DATETIME
+)');
+
+$statement = $db->prepare('INSERT INTO "visits" ("url", "time") VALUES (:url, :time)');
+$statement->bindValue(':url', ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? 'https') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+$statement->bindValue(':time', date('Y-m-d H:i:s'));
+$statement->execute();
+
+$visits = $db->querySingle('SELECT COUNT(id) FROM "visits"');
+
+echo("User visits: $visits");
+
+$db->close();
