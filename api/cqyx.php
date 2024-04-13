c4f4b75b4d4e7af70f3b3cd2cc543c913a54522b
天仙婆婆 <tqx5201@163.com>
2024-04-13 21:42:12 +0800

    Update cqyx.php

--- api/cqyx.php
+++ api/cqyx.php
@@ -1,9 +1,6 @@
 <?php
-// 添加 hosts 配置
-$hosts = array(
-    'live.cbcdn.cn:80:118.24.228.117'
-);
-
+//查看CURL的版本
+var_dump(curl_version());exit;
 //Rewrite by absentfriend
 $cityId = '5A';
 $playId= $_GET['id']??'cctv1HD';
@@ -29,42 +26,31 @@ curl_setopt_array($curl, array(
     'timestamps:'.$timestamps,
     'Content-Type: application/json;charset=utf-8'
   ),
-  // 添加 CURLOPT_RESOLVE 选项
-  //CURLOPT_RESOLVE => $hosts
   ));
 
 $response = curl_exec($curl);
-
 curl_close($curl);
 $url = (json_decode($response));
 $codes = $url->data->result->protocol[0]->transcode[0]->url;
-//exit;
+//echo $codes;//exit;
 
-$curl = curl_init();
-//$timestamps = round(microtime(true) * 1000);
-//$sign = md5('aIErXY1rYjSpjQs7pq2Gp5P8k2W7P^Y@appId' . $appId . "cityId" . $cityId. "playId" . $playId . "relativeId" . $relativeId . "timestamps" . $timestamps . "type" . $type);
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $codes,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_HTTPHEADER => array(
-    'appId: kdds-chongqingdemo',
-    'sign: '.$sign,
-    'timestamps:'.$timestamps,
-    'Content-Type: application/json;charset=utf-8'
-  ),
-  // 添加 CURLOPT_RESOLVE 选项
-  CURLOPT_RESOLVE => $hosts
-));
 
-$response = curl_exec($curl);
-curl_close($curl);
-var_dump($response);
-exit;
-header('location:'.$codes);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $codes);
+curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+curl_setopt($ch, CURLOPT_VERBOSE, 1);
+//curl_setopt($ch, CURLOPT_DNS_CACHE_TIMEOUT, 0);
+curl_setopt($ch, CURLOPT_DNS_USE_GLOBAL_CACHE, false);
+curl_setopt($ch, CURLOPT_RESOLVE, ["live.cbncdn.cn:80:118.24.228.117"]);
+//curl_setopt($ch, CURLOPT_CONNECT_TO, ["live.cbncdn.cn:80:118.24.228.117"]);
+curl_setopt($ch, CURLOPT_HEADER, true);
+//curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //如果把这行注释掉的话,就会直接输出
+$result=curl_exec($ch);
+curl_close($ch);
+
+preg_match('|Location:(.*)|',$result,$temp);
+$play_url=trim($temp[1]);
+echo $play_url;exit;
+header('location:'.$play_url);
 ?>
